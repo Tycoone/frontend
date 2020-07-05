19e688e89e05cecb5b58972b4f85f8c74cbc5ce0
NzakiCodes <ekisnzaki@gmail.com>
2020-07-05 20:50:35 +0100

    everything ok at this point< I want to get crazy

---
+++ src/app/ui/components/NavBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const NavBar = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+NavBar.propTypes = {
+
+};
+
+export default NavBar;
