8a59c3fea6ff74bb671ddcb0beb28f9e2a4fcad9
NzakiCodes <ekisnzaki@gmail.com>
2020-07-08 16:55:20 +0100

    Added Navbar

---
+++ src/pages/error/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Error = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Error.propTypes = {
+
+};
+
+export default Error;
