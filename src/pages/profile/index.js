4bc78f368509c00b0516ada0baf96a5301ec52d8
NzakiCodes <ekisnzaki@gmail.com>
2020-07-11 20:50:01 +0100

    Made Changes

---
+++ src/pages/profile/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Profile = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Profile.propTypes = {
+
+};
+
+export default Profile;
