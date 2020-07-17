e8fffe0cefb026c70145b5d251f458f973d97293
NzakiCodes <ekisnzaki@gmail.com>
2020-07-17 21:12:20 +0100

    Update index.js

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import { Grid, Card, CardMedia, CardContent, Typography, Button, Avatar } from '@material-ui/core'
-import CoverImage from '../../images/coverImage_default.jpg';
-
+import RoomIcon from '@material-ui/icons/Room';
+import { Link as RLink } from 'react-router-dom';
 const useStyles = makeStyles((theme) => ({
     root: {
         // flexGrow:1,
@@ -12,8 +12,8 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
     },
     paper: {
-        // padding: theme.spacing(10),
-
+        border: theme.palette.type === 'light' ? '1.7px solid #ededed' : '1px solid #a5acac',
+        borderRadius: theme.spacing(0.2),
         color: theme.palette.text,
         boxShadow: theme.palette.type === 'dark' ? theme.palette.elevationDark : theme.palette.elevationLight
     },
@@ -25,7 +25,6 @@ const useStyles = makeStyles((theme) => ({
     },
     media: {
         height: 140,
-        background: `url(${CoverImage})`,
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
@@ -44,33 +43,129 @@ const useStyles = makeStyles((theme) => ({
     },
     buttons: {
         padding: theme.spacing(0.7, 6),
-        textTransform:'Capitalize',
-        fontSize:17,
-        [theme.breakpoints.down('xs')]:{
+        textTransform: 'Capitalize',
+        fontSize: 17,
+        [theme.breakpoints.down('xs')]: {
             padding: theme.spacing(0.4, 4),
         }
     },
 
     buttonsfill: {
-        boxShadow: theme.palette.type==="light"? theme.palette.button.elevation.light: 'none',
+        boxShadow: theme.palette.type === 'light' ? theme.palette.button.elevation.light : 'none',
     },
+    name: {
+        fontStyle: 'normal',
+        fontWeight: 600,
+        fontSize: "20px",
+        color: theme.palette.type === 'light' ? '#333333' : '#ffffff',
+        lineHeight: '143.3%'
+    },
+    companyAndPosition: {
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: "16px",
+        color: theme.palette.type === 'light' ? '#4F4F4F' : '#ffffff',
+        lineHeight: '143.3%'
+    },
+    locationAndConnection: {
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: "16px",
+        color: theme.palette.type === 'light' ? '#4F4F4F' : '#ffffff',
+        lineHeight: '143.3%',
+        display: "flex",
+        alignItems: "flex-start"
+    },
+    bigGap: {
+        padding: theme.spacing(2)
+    },
+    aboutCard: {
+        border: theme.palette.type === 'light' ? '1.7px solid #ededed' : '1px solid #a5acac',
+        borderRadius: theme.spacing(0.2),
+        color: theme.palette.text,
+        boxShadow: theme.palette.type === 'dark' ? theme.palette.elevationDark : theme.palette.elevationLight
+    }
+    , aboutCardConent: {
+        padding: theme.spacing(4, 4),
+        position: 'relative',
+
+
+    },
+    aboutHeader: {
+        fontStyle: 'normal',
+        fontWeight: 600,
+        fontSize: "20px",
+        color: theme.palette.type === 'light' ? '#333333' : '#ffffff',
+        lineHeight: '143.3%'
+    },
+    aboutDetails: {
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: "16px",
+        color: theme.palette.type === 'light' ? '#4F4F4F' : '#ffffff',
+        lineHeight: '143.3%',
+        display: "flex",
+        alignItems: "flex-start"
+    },
+    hr: {
+        borderTop: theme.palette.type === 'light' ? '1.7px solid #ededed' : '1px solid white',
+        margin: theme.spacing(2, 0)
+    },
+    experiencePosition: {
+        fontStyle: 'normal',
+        fontWeight: 'normal',
+        fontSize: "20px",
+        color: theme.palette.type === 'light' ? '#4F4F4F' : '#ffffff',
+        lineHeight: '143.3%',
+    },
+    flex: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-between"
+    },
+    companyLogo: {
+        // alignSelf: "stretch",
+        // padding: theme.spacing(0.2),
+        border: '1px solid',
+        borderColor: theme.palette.primary.main,
+        borderRadius: "50%",
+
+
+    },
+    companyLogoImg: {
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+    },
+    profileLink:{
+        textDecoration:'none',
+        color: theme.palette.primary.main
+    }
+
+
 }))
-const Profile = () => {
+const Profile = ({ user }) => {
     const classes = useStyles();
+    const { experience } = user;
     return (
         <div className={classes.root}>
             <Grid container spacing={4}>
                 <Grid item xs={12} md={9} lg={9}>
-                    {/* <Paper className={classes.paper} elevation={false}></Paper> */}
                     <Card className={classes.paper} elevation={0}>
-                        <CardMedia className={classes.media} image="/assets/images/coverImage_default.jpg" />
+                        <CardMedia className={classes.media} image={user.images.coverImage} />
                         <CardContent className={classes.cardConent}>
-                            <Avatar alt="Profile Picture" src="/assets/images/nzaki.jpg" className={classes.small}></Avatar>
+                            <Avatar alt="Profile Picture" src={user.images.profileImage} className={classes.small}></Avatar>
                             <Grid container spacing={2}>
                                 <Grid item md={6} sm={7} xs={12} >
-                                    <Typography variant="body2" color="textSecondary" component="p">
-                                        This impressive paella is a perfect party dish and a fun meal to cook together with your
-                                        guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                                    <Typography component="h3" className={classes.name} >
+                                        {user.name.first} {user.name.last}
+                                    </Typography>
+                                    <Typography component="h5" className={classes.companyAndPosition}>
+                                        {user.currentPosition} at {user.currentCompany}
+                                    </Typography>
+                                    <Typography component="h5" className={classes.locationAndConnection}>
+                                        <RoomIcon fontSize="small" color="primary" />
+                                        {user.currentLocation} &nbsp; {user.connections}
+                                        {user.connections > 1 ? " Connections" : "Connection"}
                                     </Typography>
                                 </Grid>
                                 <Grid item md={6} sm={5} xs={12} >
@@ -99,6 +194,57 @@ const Profile = () => {
                                 </Grid>
                             </Grid>
 
+                        </CardContent>
+                    </Card>
+                    <div className={classes.bigGap} />
+                    <Card className={classes.aboutCard} elevation={0}>
+                        <CardContent className={classes.aboutCardConent}>
+                            <Typography component="h3" className={classes.aboutHeader}>
+                                About
+                            </Typography>
+                            <div className={classes.hr} />
+                            <Typography component="p" className={classes.aboutDetails}>
+                                {user.about}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+
+                    <div className={classes.bigGap} />
+                    <Card className={classes.aboutCard} elevation={0}>
+                        <CardContent className={classes.aboutCardConent}>
+                            <Typography component="h3" className={classes.aboutHeader}>
+                                Experience
+                            </Typography>
+
+                            {
+                                experience.map(item => (
+                                    <div style={{ marginBottom: 20 }}>
+                                        <div className={classes.hr} />
+                                        <Typography component="p" className={classes.experiencePosition}>
+                                            {item.position}
+                                        </Typography>
+                                        <div className={classes.flex}>
+                                            <div>
+                                                <Typography component="p" className={classes.experiencePosition}>
+
+                                                    <RLink className={classes.profileLink} to={"pages" + item.company.pageLink}> {item.company.name}</RLink>
+                                                </Typography>
+                                                <Typography component="p" className={classes.experiencePosition}>
+                                                    <i> {item.duration.from}-{item.duration.to} </i> {item.duration.count}
+                                                </Typography>
+                                            </div>
+
+                                            <div className={classes.companyLogo}>
+                                                <Avatar className={classes.companyLogoImg} alt={item.company.name + " Logo"} src={item.company.logo} ></Avatar>
+                                            </div>
+                                        </div>
+                                        <br />
+                                        <Typography component="p" className={classes.aboutDetails}>
+                                            {item.details}
+                                        </Typography>
+                                    </div>
+                                ))
+                            }
                         </CardContent>
                     </Card>
                 </Grid>
