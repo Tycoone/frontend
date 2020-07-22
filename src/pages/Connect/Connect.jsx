afdcf88b8090645b7f5bb2b169667d3c8a1bb4bc
NzakiCodes <ekisnzaki@gmail.com>
2020-07-22 15:28:50 +0100

    Update Connect.jsx

--- src/pages/Connect/Connect.jsx
+++ src/pages/Connect/Connect.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { makeStyles, Paper } from '@material-ui/core'
+import { makeStyles, Paper, Grid } from '@material-ui/core'
 import { Card, CardContent, Typography, Button, Avatar } from '@material-ui/core'
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,18 +13,28 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: theme.spacing(0.2),
     color: theme.palette.text,
     boxShadow: theme.palette.type === 'dark' ? theme.palette.elevationDark : theme.palette.elevationLight,
-    padding: theme.spacing(2,2,0,2)
+    padding: theme.spacing(2, 2, 0, 2)
+  },
+  paper2: {
+    padding: theme.spacing(3.7, 2)
+
   },
   header: {
     fontStyle: 'normal',
     fontWeight: 600,
     fontSize: "22px",
     color: theme.palette.type === 'light' ? '#333333' : '#ffffff',
+    lineHeight: '1'
+  },
+  noti: {
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: "22px",
+    color: theme.palette.type === 'light' ? '#333333' : '#ffffff',
     lineHeight: '143.3%'
   },
   hr: {
     borderTop: theme.palette.type === 'light' ? '1.7px solid #ededed' : '1px solid white',
-    margin: theme.spacing(2, 0)
   },
   buttons: {
     padding: theme.spacing(0.7, 2),
@@ -57,26 +67,161 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.type === 'light' ? '#4f4f4f' : '#ffffff',
     lineHeight: '143.3%',
   },
-  notificationAvatar: {
-    width: theme.spacing(7),
-    height: theme.spacing(7),
-    margin: theme.spacing(0, 3, 0, 0)
-  }
+  connectAvatar: {
+    width: theme.spacing(11),
+    height: theme.spacing(11),
+    margin: 'auto'
+  },
+  card: {
+    border: theme.palette.type === 'light' ? '0.09em solid #bdbdbd' : '1px solid white',
+    borderRadius: theme.spacing(0.2),
+
+  },
+  cardContent: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: theme.spacing(3)
+  },
+  name: {
+    fontStyle: 'normal',
+    fontWeight: 600,
+    fontSize: "20px",
+    color: theme.palette.type === 'light' ? '#333333' : '#ffffff',
+    textAlign: "center",
+    lineHeight: '170.3%'
+  },
+  companyAndPosition: {
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: "16px",
+    color: theme.palette.type === 'light' ? '#4F4F4F' : '#ffffff',
+    marginBottom: theme.spacing(2)
+  },
+  connections: {
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: "16px",
+    color: theme.palette.type === 'light' ? '#4F4F4F' : '#ffffff',
+    lineHeight: '143.3%',
+    display: "flex",
+    alignItems: "flex-start",
+    textAlign: "center"
+
+  },
+  centerText: {
+    textAlign: "center"
+  },
+  inviteHead: {
+    display: "flex",
+    flexWrap: "wrap",
+    padding: theme.spacing(4, 2, 0, 2)
+  },
+  bigGap: {
+    padding: theme.spacing(2)
+  },
 }));
-const Connect = () => {
+const Connect = ({ connectionRequest }) => {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.paper}>
+    <>
+      <Paper className={classes.paper}>
       <div className={classes.bigGap} />
       <Card className={classes.aboutCard} elevation={0}>
-        <CardContent className={classes.aboutCardConent}>
+        <CardContent className={classes.inviteHead}>
           <Typography component="h3" className={classes.header}>
-            Notifications
+            No New Invitations
           </Typography>
         </CardContent>
       </Card>
+
+      <div className={classes.hr} />
+      <Card elevation={0}>
+        <CardContent className={classes.aboutCardConent}>
+          <Typography component="h3" className={classes.noti}>
+            People you may know from Start Innovation Hub
+          </Typography>
+        </CardContent>
+      </Card>
+      <Grid container className={classes.paper2} spacing={4}>
+        {
+          connectionRequest.map(user => (
+            <Grid item md={3} key={user.id}>
+              <Card className={classes.card} elevation={0}>
+                <CardContent className={classes.cardContent}>
+                  <div className={classes.centerText}>
+                    <Avatar className={classes.connectAvatar} src={user.profileImage} />
+                    <Typography className={classes.name}>
+                      {user.name}
+                    </Typography>
+                    <Typography className={classes.companyAndPosition}>
+                      {user.details} <br /> 300 Connections
+                </Typography>
+                    <Typography >
+                      <Button
+                        disableElevation
+                        variant="contained"
+                        color="primary"
+                        className={classes.buttonsfill + " " + classes.buttons}
+                      >Connect</Button>
+                    </Typography>
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        }
+
+      </Grid>
     </Paper>
+    <div className={classes.bigGap} />
+    <div className={classes.bigGap} />
+
+    <Paper className={classes.paper}>
+      <div className={classes.bigGap} />
+      <Card className={classes.aboutCard} elevation={0}>
+        <CardContent className={classes.inviteHead}>
+          <Typography component="h3" className={classes.header}>
+            More Suggestion For You.
+          </Typography>
+        </CardContent>
+      </Card>
+
+      <div className={classes.hr} />
+      <Grid container className={classes.paper2} spacing={4}>
+        {
+          connectionRequest.map(user => (
+            <Grid item md={3} key={user.id}>
+              <Card className={classes.card} elevation={0}>
+                <CardContent className={classes.cardContent}>
+                  <div className={classes.centerText}>
+                    <Avatar className={classes.connectAvatar} src={user.profileImage} />
+                    <Typography className={classes.name}>
+                      {user.name}
+                    </Typography>
+                    <Typography className={classes.companyAndPosition}>
+                      {user.details} <br /> 300 Connections
+                </Typography>
+                    <Typography >
+                      <Button
+                        disableElevation
+                        variant="contained"
+                        color="primary"
+                        className={classes.buttonsfill + " " + classes.buttons}
+                      >Connect</Button>
+                    </Typography>
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        }
+
+      </Grid>
+    </Paper>
+    </>
   )
 }
 
