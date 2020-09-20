6da3d3a5f342f43060a8157098799701029772ec
NzakiCodes <ekisnzaki@gmail.com>
2020-09-20 18:01:42 +0100

    Initial

---
+++ src/views/pages/Landing/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const Landing = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+Landing.propTypes = {
+
+};
+
+export default Landing;
