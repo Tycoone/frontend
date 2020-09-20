eaf5861be3921d30b102bc0cc46d0ceb444e2fd9
NzakiCodes <ekisnzaki@gmail.com>
2020-09-20 18:02:05 +0100

    Initial

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import {style} from './index.scss';
+import './index.scss';
 
-const Navbar = ({}) => {
+const Navbar = () => {
 
     return (
-        <div className={style}>
-            
+        <div className="navbar-container">
+            <div className="logo-container">
+                <a href="/">
+                    <span>Tycoones</span>
+                </a>
+            </div>
+            <div className="page-links-container">
+                <a href="/home">Home</a>
+                <a href="/about">About Us</a>
+                <a href="/contact">Contact Us</a>
+            </div>
+            <div className="cta-links-container">
+                <a href="/signin">Sign In</a>
+                <a className="button button-primary" href="/register">Get Started</a>
+            </div>
         </div>
     );
 };
--- src/views/pages/Landing/index.js
+++ src/views/pages/Landing/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import {style} from './index.scss';
+import Navbar from '../../../components/Navbar';
+import './index.scss';
+import headerArt from '../../../assets/images/header_art.svg';
+import ImageArt from '../../../assets/images/header_art-2.svg';
 
-const Landing = ({}) => {
+const Landing = () => {
 
     return (
-        <div className={style}>
-            
+        <div>
+            <Navbar />
+            <div className="main-content">
+                <div className="header-container">
+                    <div className="header">
+                        <div className="header-image">
+                            <img src={headerArt} alt={headerArt} />
+                        </div>
+                        <div className="header-content">
+                            <h1>Moments For Startups</h1>
+                            <p>A startup is a young company founded by one or more entrepreneurs to develop a unique product or service and bring it to market.</p>
+                            <div className="cta-buttons">
+                                <a className="button button-primary" href="/register">Get Started</a>
+                                <a className="button button-outlined-primary text-secondary" href="/register">Book a Demo</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col l6">
+                            <div className="padding-tb-64 shc">
+                                <h2>Connect, Scale and Grow We’re here for you</h2>
+                                <p> A startup is a young company founded by one or more entrepreneurs to develop a unique product or service and bring it to market. A startup is a young company founded by one or more entrepreneurs to develop a unique product or ser market.</p>
+                            </div>
+                        </div>
+                        <div className="col l6">
+                            <div className="shc-img">
+                                <img src={ImageArt} alt={ImageArt} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
